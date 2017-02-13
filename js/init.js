6ae1fe811774b9ca20571c9f074a13d17b5af975
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2017-02-13 21:35:01 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function () {
+
+	$('.button-collapse').sideNav();
+	$('.parallax').parallax();
+	
+	
+	
+	
+	$(".screen-unit").css("width", screen.width);
+	$(".screen-unit").css("height", screen.height);
+	
+	
+
+}); // end of document ready
+ // end of jQuery name space
+
+
